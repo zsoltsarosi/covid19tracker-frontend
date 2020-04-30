b0f896697431b677f4c53be2e452286e00978def
zsoltsarosi <zsolt.sarosi@gmail.com>
2020-04-30 18:30:39 +0200

    World aggregated widget layout improvements

--- lib/widgets/world_aggregated_current.dart
+++ lib/widgets/world_aggregated_current.dart
@@ -14,7 +14,6 @@ class WorldAggregatedCurrent extends StatefulWidget {
 }
 
 class _WorldAggregatedCurrentState extends State<WorldAggregatedCurrent> {
-
   WorldAggregated _data;
 
   @override
@@ -31,37 +30,38 @@ class _WorldAggregatedCurrentState extends State<WorldAggregatedCurrent> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: MainData(title: "Confirmed", value: _data.confirmed, color: Theme.of(context).colorScheme.confirmed),
-              ),
-              Expanded(
-                child: MainData(title: "Recovered", value: _data.recovered, color: Theme.of(context).colorScheme.recovered),
-              ),
-              Expanded(
-                child: MainData(title: "Died", value: _data.deaths, color: Theme.of(context).colorScheme.died),
-              ),
-            ],
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: MainData(
+                      title: "Confirmed",
+                      value: _data.confirmed,
+                      color: Theme.of(context).colorScheme.confirmed),
+                ),
+                Expanded(
+                  child: MainData(
+                      title: "Recovered",
+                      value: _data.recovered,
+                      color: Theme.of(context).colorScheme.recovered),
+                ),
+                Expanded(
+                  child:
+                      MainData(title: "Died", value: _data.deaths, color: Theme.of(context).colorScheme.died),
+                ),
+              ],
+            ),
           ),
           Container(
-            margin: const EdgeInsets.only(top: 5),
             child: Row(
               children: <Widget>[
                 Expanded(
                   child: Text("Change: ${_data.increaseRate.toStringAsFixed(2)} %",
-                      textAlign: TextAlign.center,
                       style: textTheme.subtitle.copyWith(fontSize: 10.0, fontWeight: FontWeight.normal)),
                 ),
-                Expanded(
-                  child: Text(""),
-                ),
-                Expanded(
-                  child: Text(
-                      "Date: ${new DateFormat("yyyy-MM-dd").format(_data.date)}", // TODO: localized dateformat
-                      textAlign: TextAlign.right,
-                      style: textTheme.subtitle.copyWith(fontSize: 10.0, fontWeight: FontWeight.normal)),
-                ),
+                Text(
+                    "Date: ${DateFormat.yMd().format(_data.date)}",
+                    style: textTheme.subtitle.copyWith(fontSize: 10.0, fontWeight: FontWeight.normal)),
               ],
             ),
           ),
@@ -86,10 +86,8 @@ class MainData extends StatelessWidget {
     var valueStyle = textTheme.subtitle.copyWith(color: color);
     return new Container(
       padding: const EdgeInsets.all(5),
-      // color: Colors.white,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Text(this.title, style: textTheme.caption),
           SizedBox(height: 5),
