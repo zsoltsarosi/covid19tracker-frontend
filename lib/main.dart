e199188d5ed45fe1a26d4e7bce697ffc27cb3413
zsoltsarosi <zsolt.sarosi@gmail.com>
2020-05-15 17:25:09 +0200

    BLoC: using BlocDelegate for simple logging

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:covid19tracker/bloc/simple_bloc_delegate.dart';
 import 'package:covid19tracker/screens/loading_screen.dart';
 import 'package:covid19tracker/screens/main_screen.dart';
 import 'package:covid19tracker/screens/news_reader.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
