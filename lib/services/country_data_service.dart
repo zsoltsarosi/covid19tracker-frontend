32fca30c36cd1b470b515e90b71a4a7c62d9336c
zsoltsarosi <zsolt.sarosi@gmail.com>
2020-05-16 19:30:44 +0200

    CountryDataService: DataProvider base class + cache first with UTC.

--- lib/services/country_data_service.dart
+++ lib/services/country_data_service.dart
@@ -1,64 +1,73 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:covid19tracker/model/model.dart';
+import 'package:covid19tracker/services/data_provider.dart';
 import 'package:http/http.dart' as http;
-import 'package:path_provider/path_provider.dart';
 
-class CountryDataService {
+class CountryDataService extends DataProvider {
   static final CountryDataService _singleton = CountryDataService._internal();
 
   String _url = "http://10.0.2.2:54820/api/countrydata";
   String _urlDetail = "http://10.0.2.2:54820/api/countrydata/##country##";
 
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-    return directory.path;
-  }
-
-  Future<File> get _localFile async {
-    final path = await _localPath;
-    return File('$path/country_data.json');
-  }
-
   factory CountryDataService() {
     return _singleton;
   }
 
-  CountryDataService._internal();
+  CountryDataService._internal() : super(cacheThresholdInMinutes: 6 * 60, fileName: "country_data");
 
   List<CountryData> _parseData(String responseBody) {
     final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
-    
+
     return parsed.map<CountryData>((json) => CountryData.fromJson(json)).toList();
   }
 
-  Future<List<CountryData>> getData([String filter]) async {
-    List<CountryData> data = <CountryData>[];
+  Future<List<CountryData>> _requestDataAndUpdateCache() async {
+    print('Requesting data from server.');
     final response = await http.get(_url);
 
     if (response.statusCode == 200) {
-      data = _parseData(response.body);
+      var data = _parseData(response.body);
       print('Data loaded. Data points: ${data.length}. Updating cache.');
-      await _writeFile(response.body);
-
-      if (filter == null || filter.trim().isEmpty) return data;
-
-      filter = filter.trim().toLowerCase();
-      var result = data.where((country) => country.country.toLowerCase().contains(filter)).toList();
-      return result;
+      await updateCache(response.body);
+      return data;
     } else {
-      print('Error loading data: ${response.statusCode}. Trying fallback to cache.');
-
-      // trying to get cached data
-      var cachedJson = await _readFile();
-      if (cachedJson.length > 0) {
-        data = _parseData(cachedJson);
-        return data;
-      }
-
+      print('Error loading data: ${response.statusCode}.');
       throw Exception('Failed to load data');
     }
+  }
+
+  Future<List<CountryData>> getData([String filter]) async {
+    var cachedJson = await readFile();
+
+    // if cached data exists
+    if (cachedJson.length > 0) {
+      var cacheData = parseCacheData(cachedJson);
+
+      // update cache in background if it is too old
+      if (cacheData.timeStamp.difference(DateTime.now().toUtc()).inMinutes > cacheThresholdInMinutes) {
+        print('Cached data too old.');
+        _requestDataAndUpdateCache().catchError((e, s) => print(e));
+      }
+
+      print('Returning data from cache.');
+      var data = _parseData(cacheData.jsonData);
+      return _filteredData(data, filter);
+    }
+
+    // no chached data
+    print('Cached data not found.');
+    var data = await _requestDataAndUpdateCache().catchError((e, s) => print(e));
+    return _filteredData(data, filter);
+  }
+
+  List<CountryData> _filteredData(List<CountryData> data, String filter) {
+    if (data == null) return <CountryData>[];
+    if (filter == null || filter.trim().isEmpty) return data;
+
+    filter = filter.trim().toLowerCase();
+    var result = data.where((country) => country.country.toLowerCase().contains(filter)).toList();
+    return result;
   }
 
   Future<List<CountryData>> getDetail(String country) async {
@@ -72,21 +81,6 @@ class CountryDataService {
     } else {
       print('Error loading data: ${response.statusCode}.');
       throw Exception('Failed to load data');
-    }
-  }
-
-  Future<File> _writeFile(String jsonData) async {
-    final file = await _localFile;
-    return file.writeAsString(jsonData);
-  }
-
-  Future<String> _readFile() async {
-    try {
-      final file = await _localFile;
-      String contents = await file.readAsString();
-      return contents;
-    } catch (e) {
-      return "";
     }
   }
 }
