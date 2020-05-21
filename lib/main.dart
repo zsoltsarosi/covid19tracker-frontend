a7534445c1e6eaba8624ff2416dfde77860f0988
zsoltsarosi <zsolt.sarosi@gmail.com>
2020-05-21 15:41:08 +0200

    MyApp widget in a separate file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,10 @@
 import 'package:covid19tracker/bloc/simple_bloc_delegate.dart';
-import 'package:covid19tracker/screens/loading_screen.dart';
-import 'package:covid19tracker/screens/main_screen.dart';
-import 'package:covid19tracker/screens/news_reader.dart';
+import 'package:covid19tracker/widgets/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  
   BlocSupervisor.delegate = SimpleBlocDelegate();
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'COVID-19 Tracker',
-      debugShowCheckedModeBanner: false,
-      theme: new ThemeData(
-        brightness: Brightness.light,
-        primaryColorBrightness: Brightness.dark,
-        primarySwatch: Colors.blueGrey,
-      ),
-      routes: {
-        LoadingScreen.routeName: (context) => LoadingScreen(),
-        MainScreen.routeName: (context) => MainScreen(),
-        NewsReader.routeName: (context) => NewsReader()
-      },
-      initialRoute: LoadingScreen.routeName,
-    );
-  }
 }
---
+++ lib/widgets/my_app.dart
@@ -0,0 +1,26 @@
+import 'package:covid19tracker/screens/loading_screen.dart';
+import 'package:covid19tracker/screens/main_screen.dart';
+import 'package:covid19tracker/screens/news_reader.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'COVID-19 Tracker',
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        brightness: Brightness.light,
+        primaryColorBrightness: Brightness.dark,
+        primarySwatch: Colors.blueGrey,
+      ),
+      routes: {
+        LoadingScreen.routeName: (context) => LoadingScreen(),
+        MainScreen.routeName: (context) => MainScreen(),
+        NewsReader.routeName: (context) => NewsReader()
+      },
+      initialRoute: LoadingScreen.routeName,
+    );
+  }
+}
